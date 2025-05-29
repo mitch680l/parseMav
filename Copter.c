b5fad6c369a9d7b35fb4ace83a49f2876a8cdbc2
mitch115 <m395l680@ku.edu>
2025-05-29 14:56:33 +0100

    remove

--- Copter.c
+++
@@ -1,4 +0,0 @@
-#include "Copter.h"
-void Copter::executeCommand(const Command& cmd) {
-    std::cout << "[Copter] Executing command: " << cmd.getAction() << "\n";
-}
--- Plane.c
+++
@@ -1,4 +0,0 @@
-#include "Plane.h"
-void Plane::executeCommand(const Command& cmd) {
-    std::cout << "[Plane] Executing command: " << cmd.getAction() << "\n";
-}
